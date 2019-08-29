082ed21463582dc27b6eddcd316c824a7dc8c754
Adam Kendis <atkendis@gmail.com>
2019-08-28 23:06:10 -0700

    Pack info scraping loop completed.

--- webscraper.py
+++ webscraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 import os
+import csv
 
 options = webdriver.ChromeOptions()
 options.add_argument("--incognito")
@@ -22,18 +23,40 @@ try:
     WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='thumb-img pdp-link']")))
     more_button = browser.find_element_by_xpath("//span[@class='btn btn-lg btn-action']")
 
-    for i in range(0,5):
+    for _ in range(4):
         more_button.click()
-        time.sleep(5)
+        time.sleep(2)
 
     link_elements = browser.find_elements_by_xpath("//a[@class='thumb-img pdp-link']")
     links = [ele.get_attribute('href') for ele in link_elements]
     print(links)
+    time.sleep(4)
+
+    items = []
+    for i in range(len(links)):
+        browser.get(links[i])
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='js-radio-bkg-img']")))
+        name = browser.find_element_by_class_name('pdp-desktop-title').text
+        size_info = browser.find_elements_by_xpath("//tr[@class='sizedisplay-us us-sizing']")
+        size_text = [_.text for _ in size_info]
+        sizes = ['-'.join([x.split(' ')[0], x.split(' ')[len(x.split(' '))-2]]) for x in size_text]
+        final_sizes = '_'.join(sizes)
+
+        items.append([name, final_sizes])
+        time.sleep(1)
+
     browser.quit()
 
+    with open('backpacks.csv', 'w') as data:
+        writer = csv.writer(data)
+        writer.writerow(['Name', 'Sizes'])
+        writer.writerows(items)
+
 except TimeoutException:
-    print("Timed out waiting for page to finish loading")
+    print("Timed out")
     browser.quit()
+
+browser.quit()
 
 # titles_element = browser.find_elements_by_xpath("//tr[@class='sizedisplay-us us-sizing']")
 # titles_name = browser.find_element_by_class_name('pdp-desktop-title')
